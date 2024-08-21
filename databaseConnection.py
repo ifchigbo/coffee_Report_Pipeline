a69acb5de3b25c72493028cd8e4ab1c8d16cbbff
ifchigbo <ifchigbo@gmail.com>
2024-08-21 12:52:03 +0100

    updating DB async functions

--- databaseConnection.py
+++ databaseConnection.py
@@ -1,7 +1,7 @@
 import os
 import json
 from sqlalchemy import create_engine
-from sqlalchemy.ext.asyncio import  create_async_engine
+from sqlalchemy.ext.asyncio import  create_async_engine, AsyncSession
 #from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, declarative_base
 
@@ -16,14 +16,26 @@ password = content["database"]["password"]
 username = content["database"]["user"]
 database = content["database"]["database"]
 
-SQLPARAMETERS = "postgresql://{}:{}@{}/{}".format(username,password,host,database)
-engine = create_engine(
+#SQLPARAMETERS = "postgresql://{}:{}@{}/{}".format(username,password,host,database)
+SQLPARAMETERS = "postgresql+asyncpg://{}:{}@{}/{}".format(username,password,host,database)
+
+#engine = create_engine(
+#    SQLPARAMETERS
+#)
+
+engine = create_async_engine(
     SQLPARAMETERS
 )
-SessionLocal = sessionmaker(autoflush=False, autocommit=False, bind=engine)
+
+SessionLocal = sessionmaker(
+    autoflush=False,
+    autocommit=False,
+    bind=engine,
+    class_= AsyncSession
+)
 Base = declarative_base()
 
-def get_db(): #Session Maker to create sessions to the DB
+async def get_db(): #Session Maker to create sessions to the DB
     db = SessionLocal()
     try:
         yield db
